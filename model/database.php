0ac0cf7cc39a8d0f080c2d32628134d1a340ce25
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2015-01-29 09:17:06 -0800

    1st commit

---
+++ model/database.php
@@ -0,0 +1,8 @@
+<?php
+    // host that is running our server
+    $host = "localhost";
+    // username and password to be able to connect to the server   
+    $username = "root";
+    $password = "root";
+    // the name of the database, blog database
+    $database = "blog_db";
