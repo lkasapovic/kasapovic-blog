7edf2a3c3fbb819a3cccbb842911e15ec65f8bb0
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2015-02-02 10:44:26 -0800

    no message

--- controller/create-db.php
+++ controller/create-db.php
@@ -23,6 +23,23 @@
     else{
         echo "Database already exists.";
     }
+    // every post will need an 'id', a 'title', and a 'post text'
+    $query = $connection-> query("CREATE TABLE posts ("
+            // null means it cant be empty 
+           // AUTO_INCREMENT handles the id        
+            . "id int(11) NOT NULL AUTO_INCREMENT,"
+          // '255' is the minimum # of characters in a title       
+            . "title varchar(255) NOT NULL,"
+            . "post text NOT NULL,"
+          // 'PRIMARY KEY' hooks 2 tables together
+            . "PRIMARY KEY (id))");
+    
+    if($query) {
+        echo "Succesfully create table: posts";
+    }
+    else {
+        echo "<p>$connection->error</p>";
+    }
     
     $connection->close();
     
