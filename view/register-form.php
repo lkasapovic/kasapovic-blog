c9b7d23a0f9380501df3eb1a46849b920301ab95
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2015-02-17 09:20:09 -0800

    no message

--- view/register-form.php
+++ view/register-form.php
@@ -1,14 +1,21 @@
 <form>
     <div>
-        <label></label>
+        <label for="email">Email:</label>
+        <input type="text" name="email" />
     </div>
     
     <div>
-        
+        <label for="username">Username: </label>
+        <input type="text" name="username" />
     </div>
     
     <div>
-        
+        <label for="password">Password: </label>
+        <input type="password" name="password" />
+    </div>
+    
+    <div>
+        <button type="submit">Submit</button>
     </div>
 </form>
 
