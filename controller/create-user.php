4b971a5792cf1eecf32201056e65c5dbd4716848
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2015-02-19 09:18:16 -0800

    no message

--- controller/create-user.php
+++ controller/create-user.php
@@ -1,6 +1,16 @@
 <?php
     require_once(__DIR__ . "/../model/config.php");
     
-    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE);
+    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
     $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
     $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+
+    echo $password;
+  
+    $salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
+    
+    $hashedPassword = crypt($password, $salt);
+    
+    echo $hashedPassword;
+    
+    $query = $_SESSION["connection"]->query();
