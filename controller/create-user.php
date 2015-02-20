bd8012cc28ef8556d94d25b869e6632d9271943e
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2015-02-20 09:18:34 -0800

    no message

--- controller/create-user.php
+++ controller/create-user.php
@@ -5,12 +5,20 @@
     $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
     $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
-    echo $password;
   
     $salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
     
     $hashedPassword = crypt($password, $salt);
     
-    echo $hashedPassword;
+    $query = $_SESSION["connection"]->query("INSERT INTO users SET "
+            . "email = '$email',"
+            . "username = '$username',"
+            . "password = '$hashedPassword',"
+            . "salt = '$salt'");
     
-    $query = $_SESSION["connection"]->query();
+    if($query) {
+        echo "Successfully created user: $username";
+    }
+    else {
+        echo "<p>" . $_SESSION["connection"]->error . "</p>";
+    }
